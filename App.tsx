a815c804b0261d94583ce085d4fd59ae360d34aa
Alexis Frantz <loyallombardi@gmail.com>
2023-07-30 17:03:53 +0200

    wip: mainScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [text, onChangeText] = React.useState("Input Text");
+  const addATask = () => {
+    console.log("Adding Task");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Text>Tasks Created</Text>
+        <Text>Aqui apareixeran les tasks</Text>
+      </View>
+      <View>
+        <Text>Enter task</Text>
+        <TextInput
+          onChangeText={onChangeText}
+          style={{ backgroundColor: "lightblue", height: 50 }}
+        />
+      </View>
+      <View>
+        <Pressable
+          onPress={addATask}
+          style={{
+            backgroundColor: "orange",
+            borderRadius: 30,
+            height: 40,
+            width: 40,
+            justifyContent: "center",
+          }}>
+          <Text style={{ textAlign: "center", fontSize: 25, color: "white" }}>
+            +
+          </Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
